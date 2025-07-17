71f6fdd3719a28baa5a45ee8dc70aa9144f1c5c1
Ani <anirudh.krishnan@hotmail.com>
2025-07-17 23:37:43 +0100

    add collision and update pc logic

--- main.c
+++ main.c
@@ -54,6 +54,14 @@ Vector2 randomStartingVelocity(void) {
     return val;
 }
 
+bool checkPaddleCollision(Ball ball, Paddle paddle) {
+    return
+        (ball.position.x - ball.radius < paddle.position.x + paddle.width) &&
+        (ball.position.x + ball.radius > paddle.position.x) &&
+        (ball.position.y > paddle.position.y - paddle.height / 2) &&
+        (ball.position.y < paddle.position.y + paddle.height / 2);
+}
+
 void reset(GameState *state) {
     state->ball.position = (Vector2){ 800/2, 600/2 };
     state->ball.velocity = randomStartingVelocity();
@@ -75,12 +83,12 @@ int main(void) {
 
         .leftPaddle.position = (Vector2){ 5, 600/2 },
         .leftPaddle.width = 10,
-        .leftPaddle.height = 50,
+        .leftPaddle.height = 75,
         .leftPaddle.speed = 10,
 
         .rightPaddle.position = (Vector2){ 795, 600/2 },
         .rightPaddle.width = 10,
-        .rightPaddle.height = 50,
+        .rightPaddle.height = 75,
         .rightPaddle.speed = 10,
 
         .score = { 0, 0 }
@@ -93,10 +101,27 @@ int main(void) {
         // note: speed (pix/sec) * dt = pixel delta
         if (IsKeyDown(KEY_W)) state.leftPaddle.position.y -= state.leftPaddle.speed * dt * 75.0f;
         if (IsKeyDown(KEY_S)) state.leftPaddle.position.y += state.leftPaddle.speed * dt * 75.0f;
+
         
         // clamp paddle
         float halfH = state.leftPaddle.height * 0.5f;
         state.leftPaddle.position.y = fminf(fmaxf(state.leftPaddle.position.y, 0 + halfH), 600.0f - halfH);
+
+        // paddle bounce
+        float paddleEnd = state.leftPaddle.position.y + state.leftPaddle.height;
+        if (state.ball.position.x == state.leftPaddle.position.x && (state.ball.position.y >= state.leftPaddle.position.y && state.ball.position.y <= paddleEnd)) {
+            state.ball.velocity.x *= -1;
+        }
+
+        // pc control
+        if (state.ball.position.y > state.rightPaddle.position.y) {
+            state.rightPaddle.position.y += state.rightPaddle.speed * dt * 21.0f;
+        }
+        if (state.ball.position.y < state.rightPaddle.position.y) {
+            state.rightPaddle.position.y -= state.rightPaddle.speed * dt * 21.0f;
+        }
+        state.rightPaddle.position.y = fminf(fmaxf(state.leftPaddle.position.y, 0 + halfH), 600.0f - halfH);
+
         
         // ball movement
         state.ball.position.x += state.ball.velocity.x * dt;
@@ -108,6 +133,8 @@ int main(void) {
         if (state.ball.position.y < 0) state.ball.velocity.y *= -1 * scaleFactor;
 
         // paddle bounce
+        if (checkPaddleCollision(state.ball, state.leftPaddle)) state.ball.velocity.x *= -1 * scaleFactor; 
+        if (checkPaddleCollision(state.ball, state.rightPaddle)) state.ball.velocity.x *= -1 * scaleFactor;
 
         // goal: score update, ball position & velocity reset
         if (state.ball.position.x < 0) {
